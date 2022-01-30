c2c5fc2fe8ac1cf1a88657ea37d08188c3dfbccb
Maxime Durand <maximerene.durand.pro@gmail.com>
2022-01-30 11:23:01 +0100

    Mise en place de l'affichage des résultats et calcule

--- Calculatrice Ts/main.js
+++ Calculatrice Ts/main.js
@@ -1,6 +1,7 @@
 var tils = document.querySelectorAll(".numbers_tils");
 var calc_list = document.getElementById("calc_list");
 var operators = document.querySelectorAll(".calc_operator");
+var calc_result = document.getElementById("calc_result");
 tils.forEach(function (item) {
     item.addEventListener('click', function () {
         var split = item.id.split('');
@@ -18,9 +19,30 @@ var num1;
 var num2;
 var operator;
 var calc;
+function calcule() {
+    if (num1 != undefined && operator != undefined && num2 != undefined) {
+        var int1 = parseInt(num1);
+        var int2 = parseInt(num2);
+        var total = void 0;
+        if (operator == "+") {
+            total = int1 + int2;
+        }
+        else if (operator == "-") {
+            total = int1 - int2;
+        }
+        else if (operator == "*") {
+            total = int1 * int2;
+        }
+        else {
+            total = int1 / int2;
+        }
+        calc_result.innerHTML = "Result = " + total.toString();
+    }
+}
 function addToCalc(value) {
     if (value == "+" || value == "-" || value == "*" || value == "/") {
         operator = value;
+        console.log(operator);
         if (num2 == undefined) {
             calc_list.innerHTML = num1 + operator;
         }
@@ -46,4 +68,5 @@ function addToCalc(value) {
             null;
         }
     }
+    calcule();
 }
--- Calculatrice Ts/main.ts
+++ Calculatrice Ts/main.ts
@@ -1,6 +1,7 @@
 let tils : NodeListOf<Element> = document.querySelectorAll(".numbers_tils")
 let calc_list : HTMLElement = document.getElementById("calc_list")
 let operators : NodeListOf<Element> = document.querySelectorAll(".calc_operator")
+let calc_result : HTMLElement = document.getElementById("calc_result")
 
 tils.forEach(item => {
     item.addEventListener('click', function(){
@@ -22,9 +23,23 @@ let num2 : string
 let operator : string
 let calc : string
 
+function calcule () : void{
+    if(num1 != undefined && operator != undefined && num2 != undefined) { 
+        let int1 : number = parseInt(num1)
+        let int2 : number = parseInt(num2)
+        let total : number
+        if( operator == "+") { total = int1 + int2 }
+        else if ( operator == "-" ) { total = int1 - int2 } 
+        else if ( operator == "*" ) { total = int1 * int2 }
+        else { total = int1 / int2 }
+        calc_result.innerHTML = "Result = " + total.toString()
+    } 
+}
+
 function addToCalc (value : string) : void{
     if(value == "+" || value == "-" || value == "*" || value == "/"){
         operator = value
+        console.log(operator)
         if(num2 == undefined) { calc_list.innerHTML = num1 + operator }
         else { calc_list.innerHTML = num1 + operator + num2 }
         
@@ -41,4 +56,5 @@ function addToCalc (value : string) : void{
         }
         else { null }
     }
+    calcule()
 }
