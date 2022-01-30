3dbf9ace70223364828a3cbe6be8a09be5e0dec7
Maxime Durand <maximerene.durand.pro@gmail.com>
2022-01-30 10:38:21 +0100

    récupération + mise en variable de l'opérateur du calcule et affichage correct de notre calcule dans une div

--- Calculatrice Ts/main.js
+++ Calculatrice Ts/main.js
@@ -1,23 +1,49 @@
 var tils = document.querySelectorAll(".numbers_tils");
+var calc_list = document.getElementById("calc_list");
+var operators = document.querySelectorAll(".calc_operator");
 tils.forEach(function (item) {
     item.addEventListener('click', function () {
         var split = item.id.split('');
-        addNumber(parseInt(split[1]));
+        addToCalc(split[1]);
         console.log(num1, num2);
+    });
+});
+operators.forEach(function (item) {
+    item.addEventListener('click', function () {
+        var operatorType = item.getAttribute("operatorType");
+        addToCalc(operatorType);
     });
 });
 var num1;
 var num2;
 var operator;
 var calc;
-function addNumber(value) {
-    if (num1 == null) {
-        num1 = value;
-    }
-    else if (num1 != null && num2 == null) {
-        num2 = value;
+function addToCalc(value) {
+    if (value == "+" || value == "-" || value == "*" || value == "/") {
+        operator = value;
+        if (num2 == undefined) {
+            calc_list.innerHTML = num1 + operator;
+        }
+        else {
+            calc_list.innerHTML = num1 + operator + num2;
+        }
     }
     else {
-        null;
+        if (num1 == undefined) {
+            num1 = value;
+            calc_list.innerHTML = num1;
+        }
+        else if (num1 != undefined && num2 == undefined) {
+            num2 = value;
+            if (operator == undefined) {
+                null;
+            }
+            else {
+                calc_list.innerHTML = num1 + operator + num2;
+            }
+        }
+        else {
+            null;
+        }
     }
 }
--- Calculatrice Ts/main.ts
+++ Calculatrice Ts/main.ts
@@ -1,20 +1,44 @@
 let tils : NodeListOf<Element> = document.querySelectorAll(".numbers_tils")
+let calc_list : HTMLElement = document.getElementById("calc_list")
+let operators : NodeListOf<Element> = document.querySelectorAll(".calc_operator")
 
 tils.forEach(item => {
     item.addEventListener('click', function(){
         let split : string[] = item.id.split('')
-        addNumber(parseInt(split[1]))
+        addToCalc(split[1])
         console.log(num1, num2)
     })
 })
 
-let num1 : number
-let num2 : number
+operators.forEach(item => {
+    item.addEventListener('click', function(){
+        let operatorType = item.getAttribute("operatorType")
+        addToCalc(operatorType)
+    })
+})
+
+let num1 : string
+let num2 : string
 let operator : string
 let calc : string
 
-function addNumber (value : number) : void{
-    if(num1 == null){ num1 = value }
-    else if (num1 != null && num2 == null) { num2 = value}
-    else { null }
+function addToCalc (value : string) : void{
+    if(value == "+" || value == "-" || value == "*" || value == "/"){
+        operator = value
+        if(num2 == undefined) { calc_list.innerHTML = num1 + operator }
+        else { calc_list.innerHTML = num1 + operator + num2 }
+        
+    }
+    else {
+        if(num1 == undefined){ 
+            num1 = value 
+            calc_list.innerHTML = num1
+        }
+        else if (num1 != undefined && num2 == undefined) { 
+            num2 = value
+            if ( operator == undefined) { null }
+            else { calc_list.innerHTML = num1 + operator + num2 }
+        }
+        else { null }
+    }
 }
